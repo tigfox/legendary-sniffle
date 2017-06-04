9ef01c22b9f3051193558fb8a60b798faa0eb014
Eric Franson <tigfox@gmail.com>
2017-06-04 19:32:21 -0400

    thinking about inventory, attack turns

--- organic-donut/organic_donut.py
+++ organic-donut/organic_donut.py
@@ -2,12 +2,24 @@
 
 import random
 import sys
+import collections
 
 class character:
     def __init__(self, HP, AC, name):
         self.HP = HP
         self.AC = AC
         self.name = name	
+        self.inventory = []
+
+    def list_inventory():
+        self.counted_inventory = collections.Counter(self.inventory)
+        for x in self.counted_inventory:
+            thing = x
+            num = self.counted_inventory[x]
+            if num < 2:
+                print self.name + " has " + str(num) + " " + thing
+            else:
+                print self.name + " has " + str(num) + " " + thing + "s" #this is bad and I feel bad about it
 
 class inv_item:
     def __init__(self, size=1):
@@ -15,10 +27,10 @@ class inv_item:
 
 #keep inventory as a list, and check len(inventory) to make sure we're not over?
 #each item in the list is an object (an instance of a class)
-
+#if we're going to just get len(inventory), we'll need to override len for the inventory data type to sum all item sizes
 class sword(inv_item):
     def __init__(self, name, add_hp):
-        inv_item.__init__(self, 2)
+        inv_item(self, 2)
         
 
 def attack(attacker, defender):
@@ -34,8 +46,8 @@ def attack(attacker, defender):
         
         if defender.HP <= 0: #defender loses
             print defender.name + " loses."
-            sys.exit()
-        
+            return attacker, defender
+ 
         if defender.HP > 0: #defender doesn't lose
             return attacker, defender
 
@@ -53,18 +65,22 @@ def run_away(runner, striker):
         print "You escaped, you coward."
 
 def fight(player, not_player):
-    while player.HP > 0:
+    while player.HP > 0 and not_player.HP > 0:
         #decide whether we want to fight (choose run/fight)
-        action = str(raw_input("Do you want to fight or run? [Fight/Run]"))
+        action = str(raw_input("Do you want to [F]ight or [R]un?"))
         #should we choose who attacks first, or randomize it?
         #do we always allow the player to chose whether they're going to fight?
         #can they be attacked without warning?
-        if "fight" in action.lower():
+        if "f" in action.lower():
             attack(player, not_player)
-            attack(not_player, player)
-        if "run" in action.lower():
+            if not_player.HP > 0:
+                attack(not_player, player)
+        if "r" in action.lower():
             run_away(player, not_player)
-    print ("\033[1;31;40mYou Died.\n") 
+    if not_player.HP <= 0:
+        print(not_player.name + " Died.\n")
+    else:
+        print("\033[1;31;40mYou Died.\n") 
 
 
 #create your characters
@@ -82,5 +98,9 @@ fight(michael, ognut)
 #Thoughts on location:
 #each room object has four directions (NSEW), and directions with doorways/other rooms have those rooms listed.
 #Directions that are blocked should list what is blocking them (wall, locked door, etc)
+#Each room can have a method for getting the inventory of the room (most will just list inventory, some may start a fight, or list inventory that can be interacted with?)
+#
 ###
-#we also need the computer to fight back - turns.
+#upon entering a room for the first time, give a quick inventory. Entering the room subsequent times will not?
+#'look' command will give you full inventory of a room
+#'inventory' (or just 'i'?) command will show your inventory

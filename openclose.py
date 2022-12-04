e75802f2025549fd46610ba0b7768cdf2f8b38b0
Eric Franson <tigfox@gmail.com>
2022-12-03 23:54:45 -0500

    use command line arguments and check multiple symbols and truncate printing

--- openclose.py
+++ openclose.py
@@ -2,14 +2,23 @@
 
 import config
 import requests
+import argparse
 
-date_format = "%Y-%m-%d"
+parser = argparse.ArgumentParser(description="Open/Close Calculator.")
+parser.add_argument('-d', dest='days', help='Number of days from today to calculate.')
+parser.add_argument('-s', dest='shares', help='Number of shares to hold')
+parser.add_argument('-t', action='append', dest='symbols', help='The ticker symbol to calculate. Supply multiple with multiple flags.')
+parser.add_argument('-q', '--quiet', dest='quiet', action='store_true')
+args = parser.parse_args()
+symbols = args.symbols
+num_days = int(args.days)
+num_shares = int(args.shares)
+quiet = args.quiet
 
 def get_time_series(symbol):
     r = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + symbol + "&apikey=" + config.apikey)
     data = r.json()
     dailies = data['Time Series (Daily)']
-    running_total = 0.0
     return dailies
 
 def compute_closeopen(dailies, num_days):
@@ -47,18 +56,30 @@ def compute_openclose(dailies, num_days):
     return running_total
 
 def get_some_input():
-    print("This program will check for profit\nassuming you purchased shares \nat close and sold them at open \nevery working day for the last 100.\n")
-    symbol = input("What symbol do you want to check?\n")
-    num_shares = input("And how many shares will you hold?\n")
-    num_days = int(input("For many days back?\n"))
-    # do some input sanitizing
+#    print("This program will check for profit\nassuming you purchased shares \nat close and sold them at open \nevery working day for the last 100.\n")
+    symbol = input("Symbol: ")
+    num_shares = input("How many shares: ")
+    num_days = int(input("How many days: "))
+    # <fix> do some input sanitizing
     return symbol, float(num_shares), num_days
     
-
 if __name__ == "__main__":
-    symbol, num_shares, num_days = get_some_input()
-    dailies = get_time_series(symbol)
-    co_base_total = compute_closeopen(dailies, num_days)
-    oc_base_total = compute_openclose(dailies, num_days)
-    print(f"\n{num_shares} shares of {symbol} close to open\nfor the last {num_days} market days:\n${co_base_total * num_shares}")
-    print(f"\n{num_shares} shares of {symbol} open to close\nfor the last {num_days} market days:\n${oc_base_total * num_shares}")
+    print(f"computing {len(symbols)} symbols")
+     
+    #symbol, num_shares, num_days = get_some_input()
+    for symbol in symbols:
+        dailies = get_time_series(symbol)
+        co_base_total = compute_closeopen(dailies, num_days)
+        oc_base_total = compute_openclose(dailies, num_days)
+        #add truncated printing
+        if quiet:
+            print(f"{num_days} days {num_shares} {symbol} c>o: ${co_base_total * num_shares}")
+            print(f"{num_days} days {num_shares} {symbol} o>c: ${oc_base_total * num_shares}")
+        else:
+            print(f"\n{num_shares} {symbol} close to open for {num_days} market days:\n${co_base_total * num_shares}")
+            print(f"\n{num_shares} {symbol} open to close for {num_days} market days:\n${oc_base_total * num_shares}")
+
+
+
+
+
